a6d2b3f5de7e0bed169954c05eea2d525d0c5395
kangsungjin92 <76843634+kangsungjin92@users.noreply.github.com>
2021-11-05 19:39:09 +0900

    Sungjin Qna Paging

--- src/main/java/blueup/user/service/QnaService.java
+++ src/main/java/blueup/user/service/QnaService.java
@@ -2,8 +2,13 @@ package blueup.user.service;
 
 import java.util.List;
 
+import blueup.user.paging.Criteria;
 import blueup.user.vo.QnaVo;
 
 public interface QnaService {
-	public List<QnaVo> getQnaListTen();
+	public List<QnaVo> getQnaListInService(Criteria cri);
+	
+	public int getCountInService();
+	
+	public void insertQnaService(QnaVo vo);
 }
--- src/main/java/blueup/user/service/QnaServiceImpl.java
+++ src/main/java/blueup/user/service/QnaServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import blueup.user.dao.QnaDAO;
+import blueup.user.paging.Criteria;
 import blueup.user.vo.QnaVo;
 
 @Service("qnaService")
@@ -14,8 +15,23 @@ public class QnaServiceImpl implements QnaService {
 	private QnaDAO qnaDAO;
 	
 	@Override
-	public List<QnaVo> getQnaListTen() {
-		return qnaDAO.getQnaListLimitTen();
+	public List<QnaVo> getQnaListInService(Criteria cri) {
+		return qnaDAO.getQnaListInDao(cri);
 	}
+
+	@Override
+	public int getCountInService() {
+		return qnaDAO.getCountInDao();
+	}
+
+	@Override
+	public void insertQnaService(QnaVo vo) {
+		qnaDAO.insertQnaDao(vo);
+	}
+	
+	
+	
+	
+	
 
 }
